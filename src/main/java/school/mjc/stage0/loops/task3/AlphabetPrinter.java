b163e070d0ef5889730a2d23823a0aa0dfec43c8
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-31 19:05:57 +0600

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char c;
+        for (c = 'a'; c <= 'z'; c++) {
+            System.out.println(c);
+        }
     }
 }
