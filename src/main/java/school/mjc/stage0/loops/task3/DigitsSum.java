00d8ff3f5a5330ed2490b6d5336335cc1b714567
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-31 19:19:29 +0600

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int takeLastDigit;
+        int sumDigit = 0;
+        if (t < 0) {
+            t = t * -1;
+        }
+        int lengthNumber = length(t);
+        for (int iterLength = 0; iterLength <= lengthNumber; iterLength++) {
+            takeLastDigit = t % 10;
+            t = t / 10;
+            sumDigit += takeLastDigit;
+        }
+        String str = Integer.toString(sumDigit);
+        System.out.println(str);
+    }
+    
+    public static int length(int t) {
+        int length = 0;
+        int temp = 1;
+        while (temp <= t) {
+            length++;
+            temp++;
+        }
     }
 }
