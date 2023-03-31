28af02a7cf56d166fbfd0881e9415f5c066db5b8
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-31 20:36:35 +0600

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int iter = 0;
+        int result = 0;
+        while (iter <= secondBoarder) {
+            result += firstBoarder;
+            firstBoarder += firstBoarder;
+            String str = Integer.toString(result);
+            System.out.println(str);
+            iter++;
+        }
     }
 }
