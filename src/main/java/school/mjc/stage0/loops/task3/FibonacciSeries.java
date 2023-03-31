0046f14e9f5fea84a7ae8754691f6ac6bb9336e2
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-31 19:26:16 +0600

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int firstVar = 0;
+        int secondVar = 1;
+        int result;
+        for (int iter = 0; iter < lastFibonacci; iter++) {
+            String str = Integer.toString(firstVar);
+            System.out.println(str);
+            result = firstVar + secondVar;
+            firstVar = secondVar;
+            secondVar = result;
+        }
     }
 }
