8699241b5ac72f73f80b1615671ccbd7e318b353
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-31 20:33:13 +0600

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,38 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        if (first == 0) {
+            String str = Integer.toString(second);
+            System.out.println(str);
+        }
+        else if (second == 0) {
+            String str = Integer.toString(first);
+            System.out.println(str);
+        }
+        else if (first > 0 && second > 0) {
+            if (first == second) {
+                String str = Integer.toString(frist);
+                System.out.println(str);
+            }
+            else if (first > second) {
+                int resultFirst = 0;
+                for (int iter = 1; iter <= first; iter++) {
+                    if (first % iter == 0 && second % iter == 0) {
+                        resultFirst = iter;
+                    }
+                }
+                String str = Integer.toString(resultFirst);
+                System.out.println(str);
+            }
+            else if (first < second) {
+                int resultSecond = 0;
+                for (int iter = 1; iter <= second; iter++) {
+                    if (first % iter == 0 && second % iter == 0) {
+                        resultSecond = iter;
+                    }
+                }
+                String str = Integer.toString(resultSecond);
+                System.out.println(str);
+            
     }
 }
